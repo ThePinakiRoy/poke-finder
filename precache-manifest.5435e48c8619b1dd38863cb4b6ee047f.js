ea7b101b92cdde183307417e7209e33e7a68aaf1
ThePinakiRoy <roypin49@gmail.com>
2020-02-16 11:51:10 +0530

    Updates

---
+++ precache-manifest.5435e48c8619b1dd38863cb4b6ee047f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9bee261f947e7918b6dc25b7b5770dc8",
+    "url": "/poke-finder/index.html"
+  },
+  {
+    "revision": "02475b090b0dc796be6c",
+    "url": "/poke-finder/static/css/main.4313a9b5.chunk.css"
+  },
+  {
+    "revision": "6a49ae008aed481aa42c",
+    "url": "/poke-finder/static/js/2.90453ded.chunk.js"
+  },
+  {
+    "revision": "967c3aa677d48917bda81b3b2ff2693c",
+    "url": "/poke-finder/static/js/2.90453ded.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "02475b090b0dc796be6c",
+    "url": "/poke-finder/static/js/main.a7f8a32e.chunk.js"
+  },
+  {
+    "revision": "eb41e427e13bc55918c6",
+    "url": "/poke-finder/static/js/runtime-main.1483e202.js"
+  }
+]);
